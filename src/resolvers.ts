30992b9a1c8fce13fa6023de2595c077e154fca8
Eliot <a7868783@example.com>
2024-08-22 09:15:33 +0800

    first commit

---
+++ src/resolvers.ts
@@ -0,0 +1,14 @@
+
+  import { Resolvers } from './types'
+  import { DataSourceContext } from './context'
+
+  export const resolvers: Resolvers = {
+    Query: {
+      featuredListings: (_, __, {dataSources}) => {
+        return dataSources.listingAPI.getFeaturedListings();
+      },
+      listing: (_, { id }, { dataSources }) => {
+        return dataSources.listingAPI.getListing(id);
+      },
+    },
+  };
